cca289a8d98951f6f4c863e713368c8f6b75f559
Jake <57075497+cakejune@users.noreply.github.com>
2023-07-27 17:19:23 -0400

    recursive fib call

--- recursive_problems.js
+++ recursive_problems.js
@@ -164,3 +164,13 @@ function collectStrings(obj){
             }
             return -1
         }
+
+function fib(n){
+    if(n === 1){
+        return 1;
+    }
+    else if(n === 2){
+        return 1;
+    }
+    return fib(n-2) + fib(n-1)
+}
